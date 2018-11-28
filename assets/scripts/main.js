d04429667ace8db0ca726deb7ab0e7029683c2f3
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-28 16:12:54 +0100

    dus

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -74,7 +74,7 @@ var Procedure = function Procedure(el, data) {
 
         var statusAxis = d3.axisBottom(xScale);
 
-        layers.axis.append("g").attr('class', 'status-axis').attr("transform", "translate(" + -barWidth / 2 + "," + (height - config.margin.bottom) + ")").call(statusAxis);
+        layers.axis.append("g").attr('class', 'status-axis').attr("transform", "translate(" + (-barWidth / 2 + 10) + "," + (height - config.margin.bottom) + ")").call(statusAxis);
     };
 
     var renderBars = function renderBars() {
