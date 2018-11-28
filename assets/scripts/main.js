274c1f865ec3fb2023612cbdd9f0cedc83357aef
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-28 19:34:34 +0100

    dus

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -90,11 +90,7 @@ var Procedure = function Procedure(el, data) {
 
         console.log('nieuw');
 
-        var stackedData = data.map(function (d) {
-            return d.map(function (p, i) {
-                return { x: i, y: p, y0: 0 };
-            });
-        });
+        //   let stackedData = data.map(function(d) { return d.map(function(p, i) { return {x:i, y:p, y0:0}; }); });
 
         layers.bars.selectAll(".bar").selectAll('rect').data(data).enter().append('rect').attr('y', function (d) {
             return yScale(d.total);
