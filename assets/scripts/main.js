a9ac35e754b0d819812e526c10104f022986ce47
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-11-28 21:35:07 +0100

    dus

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -127,7 +127,7 @@ var Procedure = function Procedure(el, data) {
         // console.log(stackedData);
 
         var category = layers.bars.selectAll(".category").data(stackedData).enter().append("g").attr("class", function (d, i) {
-            console.log(d);return d.cat + ' category';
+            console.log(d);return d[0].cat + ' category';
         });
         // .style("fill", function(d, i) { return color(i); });
 
