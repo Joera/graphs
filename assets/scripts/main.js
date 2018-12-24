790571841e478e1654052367a5dd6f232d1c67fb
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-24 19:33:50 +0100

    apllying moduler setup with factory functions

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -277,10 +277,10 @@ var ChartStackedBars = function ChartStackedBars(config, svg, functions) {
     var redraw = function redraw(dimensions, scales) {
 
         svg.bar.attr("y", function (d) {
-            return scales.yLinear(d[1]);
-        }).attr("height", function (d) {
-            return scales.yLinear(d[0]) - scales.yLinear(d[1]);
-        }).attr("x", function (d) {
+            console.log(d);return scales.yLinear(d[1]);
+        })
+        // .attr("height", function(d) { return scales.yLinear(d[0]) - scales.yLinear(d[1]); })
+        .attr("x", function (d) {
             return scales.xBand(d.data[config.xParameter]);
         }).attr("width", scales.xBand.bandwidth());
     };
