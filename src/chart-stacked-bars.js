9dc21d0b114031d7ee9d8d893f95edd58cc3c90d
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-25 10:47:17 +0100

    apllying moduler setup with factory functions

--- src/chart-stacked-bars.js
+++ src/chart-stacked-bars.js
@@ -20,13 +20,15 @@ let ChartStackedBars = function ChartStackedBars(config,svg,functions) {
                 for (let j = 0; j < 1; j++) {  //  -   data.columns.slice(1).length - 1
                     let pathCombo = [], pathObject = {}, nextPathObject = {};
 
+                    base = base + stackedData[index][j][1];
+
                     pathObject.x = stackedData[index][j].data.status;
                     pathObject.y = stackedData[index][j][1];
                     pathObject.base = base;
                     pathObject.class = stackedData[index].key;
                     pathCombo.push(pathObject);
 
-                    base = base + stackedData[index][j][1];
+                    base = base + stackedData[index][j+1][1];
                     console.log(base);
 
                     nextPathObject.x = stackedData[index][j+1].data.status;
@@ -74,7 +76,7 @@ let ChartStackedBars = function ChartStackedBars(config,svg,functions) {
             // console.log(scales.xBand(d[0].data.status)); console.log(scales.xBand(d[1].data.status));
             .x0((d,i) => { if (i < 1) {  return  scales.xBand(d.x) + scales.xBand.bandwidth() } else { return scales.xBand(d.x);}})  // console.log(d);
             .x1((d,i) => { if (i < 1) {  return scales.xBand(d.x) + scales.xBand.bandwidth() } else { return scales.xBand(d.x); }})
-            .y0((d) => { console.log(d); return scales.yLinearReverse(d.base) + scales.yLinear(d.y); })
+            .y0((d) => { console.log(d); return scales.yLinearReverse(d.base); })
             .y1((d) => { return scales.yLinear(d.y); });
 
         svg.bar
