c7ab0f2100679164cb18e4514caa74187329ec37
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-29 21:53:42 +0100

    zzz

--- src/chart-input.js
+++ src/chart-input.js
@@ -80,7 +80,7 @@ let ChartInput = function ChartInput(config,svg,functions) {
         }
 
         svg.inputGroup
-            .attr("transform", function(d) { return 'translate(34,0)'})
+            .attr("transform", function(d) { return 'translate(30,0)'})
             .on("mouseout", unhighlight);
           //  .attr("transform", function(d) { return 'translate(64,' + scales.yInputLinear(d['cummulative'])+ ')'});
 
