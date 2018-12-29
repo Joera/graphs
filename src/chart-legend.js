8129e9e9e59b8bbe4dba44145972307008e0e226
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-29 21:22:08 +0100

    zzz

--- src/chart-legend.js
+++ src/chart-legend.js
@@ -3,7 +3,7 @@ let ChartLegend = function ChartLegend(config,svg) {
     let drawInputLegend = function drawInputLegend(dimensions) {
 
         svg.legendTotals = svg.layers.legend
-            .attr('transform', 'translate(60,' + (parseInt(dimensions.height) - 130) + ')')
+            .attr('transform', 'translate(0,' + (parseInt(dimensions.height) - 130) + ')')
             .append("text")
             .attr('class','header')
             .text('Totaal dossiers')
