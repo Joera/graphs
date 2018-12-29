5bda3f8c1eb25c8457cbbcd0ca223c35551c2213
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-29 21:16:06 +0100

    zzz

--- src/chart-legend.js
+++ src/chart-legend.js
@@ -3,31 +3,23 @@ let ChartLegend = function ChartLegend(config,svg) {
     let drawInputLegend = function drawInputLegend(dimensions) {
 
         svg.legendTotals = svg.layers.legend
-            .attr('transform', 'translate(60,' + dimensions.height + ')')
+            .attr('transform', 'translate(60,' + dimensions.height - 200 + ')')
             .append("text")
             .text('Totaal dossiers')
 
         ;
     }
 
-    let redrawtotalsLegends = function redrawtotalsLegends(dimensions,scales,axes) {
+    let redrawInputLegend = function redrawInputLegend(dimensions,scales,axes) {
 
-        axes.xBand = d3.axisBottom(scales.xBand);
 
-        // axes.xBand
-            // .ticks(d3.timeMonth.every(1))
-            // .tickFormat(d3.timeFormat("%b"));
-
-        svg.xAxis
-            .attr("transform", "translate(" + 0 + "," + dimensions.height - 200 + ")")
-            .call(axes.xBand);
     }
 
 
 
     return {
         drawInputLegend : drawInputLegend,
-        redrawtotalsLegends : redrawtotalsLegends
+        redrawInputLegend : redrawInputLegend
 
     }
 }
