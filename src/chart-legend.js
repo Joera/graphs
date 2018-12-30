5b0186b9d4a0587017a0effdbc03c11c822c6c27
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-30 10:28:59 +0100

    zzz

--- src/chart-legend.js
+++ src/chart-legend.js
@@ -3,7 +3,7 @@ let ChartLegend = function ChartLegend(config,svg) {
     let drawInputLegend = function drawInputLegend(dimensions,data) {
 
         svg.legendTotals = svg.layers.legend
-            .attr('transform', 'translate(30,' + (parseInt(config.margin.top) + 50) + ')')
+            .attr('transform', 'translate(30,' + (parseInt(config.margin.top) + 40) + ')')
 
         svg.legendTotals.append("text")
             .attr('class','header')
