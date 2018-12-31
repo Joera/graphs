3368dcd56c0d9668d9b4e57d72688edb8bbb74d4
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 23:55:43 +0100

    massackre

--- src/chart-blocks.js
+++ src/chart-blocks.js
@@ -5,6 +5,8 @@ let ChartBlocks = function ChartBlocks(config,svg,functions) {
         // manipulate the data into stacked series
         // format data for areaflow
 
+        console.log(data);
+
         let blocksArray = function(d) {
 
             let noBlocks = Math.round(parseInt(d[config.yParameter]) / 100);
