865752af867139a472ea325366644ebe1babacd8
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 21:52:00 +0100

    massackre

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -127,7 +127,8 @@ var TCMGCharts = function TCMGCharts() {
 
         config.margin.top = 0;
         config.margin.bottom = 30;
-        config.margin.left = 30;
+        config.margin.left = 40;
+        config.margin.right = 0;
         config.padding.top = 25;
         config.padding.bottom = 30;
         config.padding.left = 0;
