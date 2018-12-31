146b0c69778476186b53ef1faada013dac27b5fa
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 21:02:45 +0100

    massackre

--- src/chart-scales.js
+++ src/chart-scales.js
@@ -10,7 +10,7 @@ let ChartScales = function ChartScales(config,dimensions,scales) {
         scales.yLinear = d3.scaleLinear()
             // .range([dimensions.height, config.margin.top + config.padding.top])
             // .domain([0,d3.max(data, d => d[config.yParameter])]).nice();
-            .range([(2 * 259), 0]) // geen idee waarom 259 ipv 250
+            .range([(259), 0]) // geen idee waarom 259 ipv 250
             .domain([0,12500]).nice();
 
         scales.xBand = d3.scaleBand()
