4eb150ce473df5ae7a9e36315cb3f635eac6a53a
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 19:51:09 +0100

    massackre

--- src/chart-scales.js
+++ src/chart-scales.js
@@ -23,7 +23,7 @@ let ChartScales = function ChartScales(config,dimensions,scales) {
             .align([0.5]);
 
         scales.yInputLinear = d3.scaleLinear()
-            .range([dimensions.height, 300])
+            .range([dimensions.height, 0])
             .domain([0,25000]).nice();
 
         return scales;
