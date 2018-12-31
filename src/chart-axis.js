4e5d0b0fb5f6fe6e88f178907e2f7bc4afb0ba18
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 17:13:31 +0100

    massackre

--- src/chart-axis.js
+++ src/chart-axis.js
@@ -42,7 +42,7 @@ let ChartAxis = function ChartAxis(config,svg) {
 
         svg.yInputAxis = svg.layers.axes.append("g")
             .attr('class', 'y-axis')
-            .attr("transform", "translate(" + (parseInt(config.margin.left) + 0) + ","  + (0) + ")");
+            .attr("transform", "translate(" + (parseInt(config.margin.left) + 0) + ","  + (config.margin.top) + ")");
     }
 
     let redrawInputYAxis = function redrawInputYAxis(scales,axes) {
