119229821c33d142d64ec8365cf01d5dfd10fec8
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 21:22:04 +0100

    massackre

--- src/chart-scales.js
+++ src/chart-scales.js
@@ -10,8 +10,8 @@ let ChartScales = function ChartScales(config,dimensions,scales) {
         scales.yLinear = d3.scaleLinear()
             // .range([dimensions.height, config.margin.top + config.padding.top])
             // .domain([0,d3.max(data, d => d[config.yParameter])]).nice();
-            .range([(259), 0]) // geen idee waarom 259 ipv 250
-            .domain([0,12500]).nice();
+            .range([(155), 0]) // geen idee waarom 259 ipv 250
+            .domain([0,15000]).nice();
 
         scales.xBand = d3.scaleBand()
             // what is domain when working with a stack?
