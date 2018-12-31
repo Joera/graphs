51520e14be5fa6519b2ef4f8628f3d7625c87933
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-01 00:03:17 +0100

    massackre

--- src/chart-blocks.js
+++ src/chart-blocks.js
@@ -10,6 +10,7 @@ let ChartBlocks = function ChartBlocks(config,svg,functions) {
         let blocksArray = function(d) {
 
             let noBlocks = Math.round(parseInt(d[config.yParameter]) / 100);
+            console.log(noBlocks);
             let arr = new Array(noBlocks);
 
             for (let i = 0; i < arr.length; i++) {
