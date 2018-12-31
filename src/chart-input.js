795bfe588dbeef5c4680c9048420e9f00e73510a
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 17:30:37 +0100

    massackre

--- src/chart-input.js
+++ src/chart-input.js
@@ -81,7 +81,7 @@ let ChartInput = function ChartInput(config,svg,functions) {
         }
 
         svg.inputGroup
-            .attr("transform", function(d) { return 'translate(64,' + (config.margin.top + config.padding.top + 70) + ')'})
+            .attr("transform", function(d) { return 'translate(64,' + (config.margin.top + config.padding.top + 50) + ')'})
             .on("mouseout", unhighlight);
           //  .attr("transform", function(d) { return 'translate(64,' + scales.yInputLinear(d['cummulative'])+ ')'});
 
