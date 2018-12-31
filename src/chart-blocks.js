7e5535cffbba1b702cfe216606c829189a9bcf11
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 20:53:51 +0100

    massackre

--- src/chart-blocks.js
+++ src/chart-blocks.js
@@ -69,9 +69,9 @@ let ChartBlocks = function ChartBlocks(config,svg,functions) {
 
                 let s = Math.ceil((parseInt(i) + (parseInt(d.previous) / 100))).toString();
                 if (s.length > 1) {
-                    return config.padding.top  - 4 - (10 * parseInt(s.substring(0,s.length - 1)));
+                    return config.padding.top  - 2 - (10 * parseInt(s.substring(0,s.length - 1)));
                 } else {
-                    return config.padding.top - 4; }
+                    return config.padding.top - 2; }
             })
             .on("mouseover", (d,i) => {
                 highlight(d.provenance);
