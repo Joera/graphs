8e127109e574d358e1e0160af86429662eb7f7c8
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-01 00:36:35 +0100

    massackre

--- src/chart-blocks.js
+++ src/chart-blocks.js
@@ -41,10 +41,10 @@ let ChartBlocks = function ChartBlocks(config,svg,functions) {
 
                 for (let i = 0; i < arr.length; i++) {
                     arr[i] = {};
-                    arr[i].previous = d.previous;
-                    arr[i].total = d.total;
-                    arr[i].cummulative = d.cummulative;
-                    arr[i].provenance = d.provenance;
+                    arr[i].previous = 0;
+                    arr[i].total = d.value;
+                    arr[i].cummulative = 0;
+                    arr[i].provenance = d.status;
                 }
 
                 return arr;
