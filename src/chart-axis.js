1bf1ec75d95b88b638fef98c5579b5e127ec66f1
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-31 22:08:16 +0100

    massackre

--- src/chart-axis.js
+++ src/chart-axis.js
@@ -15,7 +15,7 @@ let ChartAxis = function ChartAxis(config,svg) {
             // .tickFormat(d3.timeFormat("%b"));
 
         svg.xAxis
-            .attr("transform", "translate(" + 0 + "," + (310 + config.margin.top + config.padding.top) + ")")  //
+            .attr("transform", "translate(" + config.padding.left + "," + (310 + config.margin.top + config.padding.top) + ")")  //
             .call(axes.xBand);
     }
 
