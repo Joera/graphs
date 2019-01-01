598a742b22305f23f7d1f91378451cd2356da501
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-01 15:50:44 +0100

    massackre

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -296,7 +296,7 @@ var TCMGCharts = function TCMGCharts() {
             chartSVG.redraw(dimensions);
 
             svg.layers.data.append('text')
-                .text(d3.format('€,')(data[0].value))
+                .text(d3.format('$,')(data[0].value))
                 .attr("text-anchor","middle")
                 .attr("x","50%");
             ;
