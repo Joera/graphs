997c40c40d3978877f419f474c6d9d7831a2f3fd
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-01 16:05:32 +0100

    massackre

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -6,7 +6,7 @@
  */
 
 
-const locale = d3.formatDefaultLocale({
+const localCurrency = d3.formatDefaultLocale({
     "decimal": ",",
     "thousands": ".",
     "grouping": [3],
@@ -21,7 +21,22 @@ const locale = d3.formatDefaultLocale({
     "shortMonths": ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"]
 });
 
-// const formatDates = locale.format("%B %Y");
+const localTime = d3.timeFormatLocale({
+    "decimal": ",",
+    "thousands": ".",
+    "grouping": [3],
+    "currency": ["€",""],
+    "dateTime": "%a %e %B %Y %T",
+    "date": "%d-%m-%Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"],
+    "shortDays": ["zo", "ma", "di", "wo", "do", "vr", "za"],
+    "months": ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
+    "shortMonths": ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"]
+});
+
+const formatDates = localTime.format("%B %Y");
 
 
 
@@ -295,7 +310,7 @@ var TCMGCharts = function TCMGCharts() {
             dimensions = chartDimensions.get(dimensions);
             chartSVG.redraw(dimensions);
 
-            var currency = locale.format("$,.2f");
+            var currency = localCurrency.format("$,.2f");
 
             svg.layers.data.append('text')
                 .text(currency(data[0].value))
