754ec1e46e282d4431289ff567d345a079eb1ad7
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-01 18:58:16 +0100

    massackre

--- src/chart-bar.js
+++ src/chart-bar.js
@@ -16,12 +16,12 @@ let ChartBar = function ChartBar(config,svg) {
         svg.bar
             .attr("x", function(d) { return scales.xBand(d[config.xParameter]); })
             .attr("y", function(d) { return scales.yLinear(d[config.yParameter]); })
-        //     .attr("height", function(d) { return dimensions.height - scales.yLinear(d[config.xParameter]); })
+            .attr("height", function(d) { return dimensions.height - scales.yLinear(d[config.yParameter]); })
             .attr("width", barWidth)
         ;
     }
 
-        
+
     return  {
         draw : draw,
         redraw : redraw
