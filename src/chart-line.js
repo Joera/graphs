328c52b4a83a565bbb08e7a0003a518205d62b7e
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-01 10:55:50 +0100

    massackre

--- src/chart-line.js
+++ src/chart-line.js
@@ -10,7 +10,7 @@ let ChartLine = function ChartLine(config,svg) {
     let redraw = function redraw(scales,functions) {
 
         functions.line = d3.line()
-            .x(function(d) { return scales.xTime(new Date(d.date)); })
+            .x(function(d) { return scales.xTime(new Date(d.time)); })
             .y(function(d) { return scales.yLinear(d[config.yParameter]); });
 
         svg.line
