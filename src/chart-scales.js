c26d65ae73cad81ffa4affdf0044f12d5e807ac3
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-01 20:18:30 +0100

    massackre

--- src/chart-scales.js
+++ src/chart-scales.js
@@ -36,7 +36,7 @@ let ChartScales = function ChartScales(config,dimensions,scales) {
 
         newScales.xBand
             // or does this
-            .range([config.padding.left, dimensions.width])
+            .range([config.padding.left, dimensions.width + config.padding.left])
 
         return newScales;
     }
