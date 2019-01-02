bde72f720b6f5cd5f270c95a3e48503c979f95a5
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-02 12:44:11 +0100

    massackre

--- src/chart-dimensions.js
+++ src/chart-dimensions.js
@@ -6,9 +6,9 @@ let ChartDimensions = function ChartDimensions(element,config) {
         dimensions.containerWidth = d3.select(element).node().getBoundingClientRect().width - config.margin.left - config.margin.right;
         dimensions.width = dimensions.containerWidth - config.padding.left - config.padding.right;
 
-        if(config.fixedHeight) {
-            dimensions.containerHeight = config.fixedHeight + config.padding.top + config.padding.bottom;
-            dimensions.height = config.fixedHeight;
+        if(config.maxValue) {
+            dimensions.containerHeight = (config.maxValue / 100) + config.padding.top + config.padding.bottom;
+            dimensions.height = (config.maxValue / 100);
         } else {
             dimensions.containerHeight = d3.select(element).node().getBoundingClientRect().height - config.margin.top - config.margin.bottom;
             dimensions.height = dimensions.containerHeight - config.padding.top - config.padding.bottom;
