daed095b15eaa05e85e5db012a9ecdc7b7fccc82
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-13 11:11:17 +0100

    yoyo

--- src/chart-stacked-area.js
+++ src/chart-stacked-area.js
@@ -24,7 +24,7 @@ let ChartStackedArea = function ChartStackedBars(config,svg,functions) {
 
         let area = d3.area()
             .x(function(d) {
-                return scales.xTime(d.data.date); })
+                return scales.xTime(new Date(d.data.date)); })
             .y0(function(d) { return scales.yLinear(d[0]); })
             .y1(function(d) { return scales.yLinear(d[1]); });
 
