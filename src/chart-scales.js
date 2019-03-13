3732836235529580c48878966bd2749ca3dd98c1
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-13 14:55:48 +0100

    yoyo

--- src/chart-scales.js
+++ src/chart-scales.js
@@ -19,7 +19,7 @@ let ChartScales = function ChartScales(config,dimensions,scales) {
         scales.xBand = d3.scaleBand()
             // what is domain when working with a stack?
             .domain(data.map(d => d[config.xParameter]))
-            // .paddingInner(config.paddingInner)
+            .paddingInner(config.paddingInner)
             // .paddingOuter([0])
             // .align(config.xAlign)
         ;
