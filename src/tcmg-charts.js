261096604de11c1e38a51ff6f844c1587aabfd3c
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-13 12:55:09 +0100

    yoyo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -331,6 +331,90 @@ var TCMGCharts = function TCMGCharts() {
         });
     }
 
+    var ProgressCandles = function(element) {
+
+        let chartObjects = ChartObjects();
+        let config = chartObjects.config();
+        let dimensions = chartObjects.dimensions();
+        let svg = chartObjects.svg();
+        let scales = chartObjects.scales();
+        let axes = chartObjects.axes();
+        let functions = chartObjects.functions();
+
+        config.margin.top = 0;
+        config.margin.bottom = 0;
+        config.margin.left = 0;
+        config.margin.right = 0;
+        config.padding.top = 30;
+        config.padding.bottom = 30;
+        config.padding.left = 40;
+        config.padding.right = 0;
+        // name of first column with values of bands on x axis
+
+
+        config.yParameter = 'behandeling';  // is being set in type function
+        config.fixedHeight = 160;
+        config.minValue = 0;
+        config.maxValue = 22000;
+
+
+        config.xParameter = 'date';
+        config.minWidth = 460;
+
+
+
+        // get dimensions from parent element
+        let chartDimensions = ChartDimensions(element,config);
+        dimensions = chartDimensions.get(dimensions);
+
+        // create svg elements without data
+        let chartSVG = ChartSVG(element,config,dimensions,svg);
+        let chartScales = ChartScales(config,dimensions,scales);
+        let chartAxis = ChartAxis(config,svg);
+        let chartLine = ChartLine(config,svg,functions);
+        let chartStackedArea = ChartStackedArea(config,svg,functions);
+
+        chartAxis.drawXAxis();
+        chartAxis.drawYAxis();
+
+        d3.csv("./dummy_data_progress_extended.csv", function(error, csv) {
+            if (error) throw error;
+
+            //       let neededColumns = ['date','aos','besluiten','inbehandeling','meldingen','opnames'];
+            let neededColumns = ['date','behandeling','afgehandeld'];
+
+            let data = trimColumns(csv,neededColumns);
+            // csv.columns = [csv.columns[0],csv.columns[2],csv.columns[1]];
+
+            functions.stack = d3.stack();
+            let stackedData = functions.stack.keys(data.columns.slice(1))(data);
+
+            function redraw() {
+                // on redraw chart gets new dimensions
+                dimensions = chartDimensions.get(dimensions);
+                chartSVG.redraw(dimensions);
+                // new dimensions mean new scales
+                scales = chartScales.reset(dimensions,scales);
+                // new scales mean new axis
+                chartAxis.redrawXTimeAxis(dimensions,scales,axes);
+                chartAxis.redrawYAxis(scales,axes);
+                // redraw data
+                chartLine.redraw(scales,functions);
+                chartStackedArea.redraw(dimensions,scales);
+
+            }
+
+            scales = chartScales.set(data);
+            chartLine.draw(data);
+            chartStackedArea.draw(stackedData,functions);
+            // further drawing happens in function that can be repeated.
+            redraw();
+            // for example on window resize
+            window.addEventListener("resize", redraw, false);
+
+        });
+    }
+
     var Remitted = function Remitted(element) {
 
         let chartObjects = ChartObjects();
@@ -527,6 +611,7 @@ var TCMGCharts = function TCMGCharts() {
         legend : Legend,
         procedure : Procedure,
         progress : Progress,
+        progressCandles : ProgressCandles,
         remitted : Remitted,
         outputs : Outputs,
         remittances : Remittances
