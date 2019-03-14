73b44ed8479a27707cde2b7d6b5a1db1710d4cc2
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-14 19:46:41 +0100

    yo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -238,7 +238,7 @@ var TCMGCharts = function TCMGCharts() {
                         return 'orange';
                     })
                     .attr("fill-opacity", function (d, i) {
-                        return i / 10;
+                        return d.properties.totaal / 1517;
                     })
                     .attr("class", function (d, i) {
                         return sluggify(d.properties.name);
