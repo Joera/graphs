a5b9e0894f90d0cf8e13c7b8e18381d370d74498
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-14 17:53:56 +0100

    yo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -210,8 +210,8 @@ var TCMGCharts = function TCMGCharts() {
 
              console.log(b);
             projection
-                .scale(2000)
-                .translate([0,2500])
+                .scale(2400)
+                .translate([0,2000])
             ;
 
             svg.layers.data.selectAll("path")
