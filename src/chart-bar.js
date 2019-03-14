d6f860d3baec807e04d992986f057bd8b8daf081
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-14 10:10:42 +0100

    yoyo

--- src/chart-bar.js
+++ src/chart-bar.js
@@ -47,9 +47,11 @@ let ChartBar = function ChartBar(config,svg) {
         // let barWidth = ((dimensions.width - config.padding.left - config.padding.right) / data.length) - 2;
 
 
+        console.log(svg.bar);
+
 
         svg.bar
-      
+            .merge(svg.bar)
             .attr("x", function(d) { return scales.xBand(d[config.xParameter]); })
             .attr("y", function(d) { return dimensions.height; })
             .attr("height", 0)
