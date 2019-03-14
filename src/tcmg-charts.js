a83990fb86ef6042d6ced04b9c91b1ece16f5c8d
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-14 14:25:28 +0100

    yo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -730,7 +730,7 @@ var TCMGCharts = function TCMGCharts() {
         let dimensions = chartObjects.dimensions();
         let svg = chartObjects.svg();
 
-        config.margin.top = 0;
+        config.margin.top = 10;
         config.padding.left = 60;
         config.padding.bottom = 30;
         config.margin.bottom = -30;
