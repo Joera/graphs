1eec276fef4a97b906367de02d5f55aea34770f5
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-14 16:45:48 +0100

    yo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -193,7 +193,7 @@ var TCMGCharts = function TCMGCharts() {
             var l = topojson.feature(nld, nld.objects.subunits).features[3],
                 b = path.bounds(l),
                 s = .2 / Math.max((b[1][0] - b[0][0]) / dimensions.containerWidth, (b[1][1] - b[0][1]) / dimensions.height),
-                t = [(dimensions.containerWidth - s * (b[1][0] + b[0][0])), (dimensions.height - s * (b[1][1] + b[0][1]))];
+                t = [(dimensions.containerWidth - s * (b[1][0] + b[0][0])), (dimensions.height - s * (b[1][1] + b[0][1])) / 4];
 
             projection
                 .scale(s)
