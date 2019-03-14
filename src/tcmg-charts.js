3adcb727466ad2160d8cc224be1c4cd3347b86ac
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-14 16:51:25 +0100

    yo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -181,9 +181,9 @@ var TCMGCharts = function TCMGCharts() {
 
         let colour = d3.scaleOrdinal(d3.schemeCategory10);
 
-        let projection = d3.geoMercator()
-            .scale(1)
-            .translate([0, 0]);
+        let projection = d3.geoMercator();
+            // .scale(1)
+            // .translate([0, 0]);
 
         let path = d3.geoPath()
             .projection(projection);
