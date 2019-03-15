b9fb49bb3ed6a26992a2a4f40bd5eb318aef8c9f
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-15 11:01:08 +0100

    yo

--- src/tcmg-charts-v2.js
+++ src/tcmg-charts-v2.js
@@ -65,12 +65,12 @@ var TCMGCharts = function TCMGCharts() {
 
         config.margin.top = 0;
         config.margin.bottom = 0;
-        config.margin.left = 60;
+        config.margin.left = 0;
         config.margin.right = 0;
         config.padding.top = 10;
         config.padding.bottom = 30;
 
-        config.padding.left = 0;
+        config.padding.left = 60;
         config.padding.right = 60;
         config.xParameter = 'provenance';  // name of first column with values of bands on x axis
         config.yParameter = 'total';  // is being set in type function
