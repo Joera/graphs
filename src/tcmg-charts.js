ab0c0352cbd058ce1658ac5f2d9d3008a1529cef
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-18 18:46:32 +0100

    nwe grenzen

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -319,7 +319,7 @@ var TCMGCharts = function TCMGCharts() {
                 t = [(dimensions.containerWidth - s * (b[1][0] + b[0][0])) / 2, ((dimensions.height - s * (b[1][1] + b[0][1])) / 2) - 40];
 
             projection
-                .scale(s / 100)
+                .scale(s)
                 .translate(t)
             ;
 
