4959a20c6b141099fc2b7d9f99c812202eb369f9
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-03-19 10:09:02 +0100

    waat

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -296,14 +296,13 @@ var TCMGCharts = function TCMGCharts() {
         dimensions = chartDimensions.get(dimensions);
 
         let projection = d3.geoMercator()
-            .scale(1)
-            .translate([0, 0]);
+            .translate([ parseInt(dimensions.containerWidth) / 2, parseInt(dimensions.height)  / 2]);
 
-
+        let path = d3.geoPath()
+            .projection(projection);
 
         let chartSVG = ChartSVG(element, config, dimensions, svg);
 
-        dimensions = chartDimensions.get(dimensions);
         chartSVG.redraw(dimensions);
 
         d3.json("/assets/geojson/single_gemeente_II.geojson", function (error, geojson) {
@@ -313,21 +312,18 @@ var TCMGCharts = function TCMGCharts() {
             //     s = .2 / Math.max((b[1][0] - b[0][0]) / dimensions.containerWidth, (b[1][1] - b[0][1]) / dimensions.height),
             //     t = [(dimensions.containerWidth - s * (b[1][0] + b[0][0])), (dimensions.height - s * (b[1][1] + b[0][1])) / 2];
 
-            console.log(geojson.bbox);
-
-            var l = geojson.features[0],
-                b = path.bounds(geojson.features[0]),
-                s = .2 / Math.max((b[1][0] - b[0][0]) / dimensions.containerWidth, (b[1][1] - b[0][1]) / dimensions.height),
-                t = [(dimensions.containerWidth - s * (b[1][0] + b[0][0])) / 2, ((dimensions.height - s * (b[1][1] + b[0][1])) / 2) - 40];
-
-            console.log(b);
-            projection
-                .scale(s)
-                .translate(t)
-            ;
-
-            let path = d3.geoPath()
-                .projection(projection);
+            // console.log(geojson.bbox);
+            //
+            // var l = geojson.features[0],
+            //     b = path.bounds(geojson.features[0]),
+            //     s = .2 / Math.max((b[1][0] - b[0][0]) / dimensions.containerWidth, (b[1][1] - b[0][1]) / dimensions.height),
+            //     t = [(dimensions.containerWidth - s * (b[1][0] + b[0][0])) / 2, ((dimensions.height - s * (b[1][1] + b[0][1])) / 2) - 40];
+            //
+            // console.log(b);
+            // projection
+            //     .scale(s)
+            //     .translate(t)
+            // ;
 
             d3.csv("./dummy_data_map_output.csv", function (error, csv) {
                 if (error) throw error;
@@ -357,7 +353,7 @@ var TCMGCharts = function TCMGCharts() {
                         return 'orange';
                     })
                     .attr("fill-opacity", function (d, i) {
-                        return i;
+                        return i / 10;
                     })
                     .attr("class", function (d, i) {
                         return sluggify(d.properties.gemeentenaam);
