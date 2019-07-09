fa3d48a8130472b18f58f67458db03d9f7c30688
Joera <joeramulders@gmail.com>
2019-07-09 15:55:05 +0200

    yo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -487,7 +487,7 @@ var TCMGCharts = function TCMGCharts() {
 
                 function prepareData(json) {
 
-                    console.log(json['ONTVANGST']);
+                    console.log(json);
 
                     let data = [];
 
