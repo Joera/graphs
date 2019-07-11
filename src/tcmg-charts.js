80c71150b89bf376a67cc5537d9bcf6efaceaf04
Joera <joeramulders@gmail.com>
2019-07-11 14:45:37 +0200

    yo

--- src/tcmg-charts.js
+++ src/tcmg-charts.js
@@ -1191,6 +1191,10 @@ var TCMGCharts = function TCMGCharts() {
             console.log(json);
             let columns = Object.keys(json[0]);
 
+            columns.filter( col => {
+
+                return ['id','DATUM','CATEGORY','_category','_week','_year'].indexOf(col) < 0;
+            })
 
             console.log(columns);
 
