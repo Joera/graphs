fda2b9311193dbdeb6619648fd6d6d8daa2d25cb
Joera <joeramulders@gmail.com>
2019-09-06 13:35:05 +0200

    relevant graphs

--- src/chart-stacked-bars.js
+++ src/chart-stacked-bars.js
@@ -192,7 +192,7 @@ let ChartStackedBars = function ChartStackedBars(config,svg,functions) {
 
                     xOffset = dimensions.width / (2 * dataArray.length);
 
-                    yOffset = (scales.yLinear(d[0] - config.minValue) - scales.yLinear(d[1] - config.minValue)) / 2);
+                    yOffset = (scales.yLinear(d[0] - config.minValue) - scales.yLinear(d[1] - config.minValue) / 2);
 
                     return 'translate(' + (scales.xTime(new Date(d.data[config.xParameter])) + xOffset  + ',' +
                         (scales.yLinear(d[1]) + yOffset)
