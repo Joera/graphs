e153f21bdadf0b43d91edfcb94a5083cd1a3de5c
Joera <joeramulders@gmail.com>
2019-09-06 13:21:55 +0200

    relevant graphs

--- src/chart-stacked-bars.js
+++ src/chart-stacked-bars.js
@@ -189,7 +189,7 @@ let ChartStackedBars = function ChartStackedBars(config,svg,functions) {
                 if (config.xParameter === '_date') {
 
                     return 'translate(' + (scales.xTime(new Date(d.data[config.xParameter]))) + dimensions.width / (2 * dataArray.length) + ',' +
-                        scales.yLinear(d[1]) - 100
+                        (scales.yLinear(d[1]) - 100)
                         + ')';
 
                 } else {
