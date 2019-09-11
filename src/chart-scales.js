5c9c2275ed56cd583438ddb1afb60332f0b6cbab
Joera <joeramulders@gmail.com>
2019-09-11 21:12:50 +0200

    mapz

--- src/chart-scales.js
+++ src/chart-scales.js
@@ -34,7 +34,7 @@ let ChartScales = function ChartScales(config,dimensions,scales) {
 
         scales.xBandStacked = d3.scaleBand()
         // what is domain when working with a stack?
-            .domain(data.map( function(d) { console.log(d); return d[config.xParameter] } ))
+            .domain(data.map( function(d) { console.log(d.data['_date']); return d.data[config.xParameter] } ))
             .paddingInner(config.paddingInner)
             .paddingOuter(config.paddingOuter)
             .align([0.5])
