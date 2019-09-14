d03a13818ae59c06ea8a7750047d2aa152630056
Joera <joeramulders@gmail.com>
2019-09-14 20:24:33 +0200

    stylin

--- src/chart-sankey.js
+++ src/chart-sankey.js
@@ -362,7 +362,7 @@ let ChartSankey = function ChartSankey(config,svg) {
                 }
             })
             .attr("d", shortPath)
-            .style("stroke-width", function(d) { return 4 } );
+            .style("stroke-width", function(d) { return 1 } );
             // .sort(function(a, b) { return b.dy - a.dy; });
 
 
