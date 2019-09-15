c39362fe8e0dad28a43e2cc41fd2bcb749fbb723
Joera <joeramulders@gmail.com>
2019-09-15 09:57:32 +0200

    stylin

--- src/default/stroommodel.js
+++ src/default/stroommodel.js
@@ -32,7 +32,51 @@ var stroommodel = function(element) {
 
     let chartSankey = ChartSankey(config,svg,functions);
 
-    d3.json(url, function(error, {result,filteredObject,nodes,links}) {
+    let connections = [
+
+        ["VOOR_19MRT", "VOOR_19MRT_MET_HISTORIE", "HISTORIE",""],
+        ["VOOR_19MRT", "VOOR_19MRT_GEEN_HISTORIE", "MELDING",""],
+        ["VOOR_19MRT", "AOS_VR_19MRT_MET_HISTORIE", "aos_totaal",""],
+
+        ["19MRT_30SEPT", "19MRT_TM_30SEPT_GEEN_HIST", "MELDING",""],
+        ["19MRT_30SEPT", "19MRT_TM_30SEPT_MET_HISTO", "HISTORIE",""],
+        ["19MRT_30SEPT", "AOS_REGULIER_19MRT_30SEPT", "aos_totaal",""],
+
+        ["VANAF_O1OKT2018","VANAF_O1OKT2018_GEEN_HIST", "MELDING",""],
+        ["VANAF_O1OKT2018","VANAF_010KT2018_MET_HISTO","HISTORIE",""],
+        ["VANAF_O1OKT2018","AOS_REGULIER_VANAF_1OKT20","aos_totaal",""],
+
+        //  ["GEEN_HISTORIE", "GEEN_HISTORIE", "MELDING"],
+        ["HISTORIE", "HISTORIE", "MELDING",""],
+        ["MELDING", "OPNAMES", "OPNAMES",""],
+        ["MELDING", "AANTAL_BESLUIT_STUWMEER", "STUWMEERREGELING","completed"],
+        ["MELDING", "GEEN_REACTIE_STUWMEER", "STUWMEERREGELING","uncompleted"],
+        ["MELDING","WACHT_OP_OPNAME","IN_PROCEDURE","in-procedure"],
+        ["STUWMEERREGELING", "GEEN_REACTIE_STUWMEER", "IN_PROCEDURE","in-procedure"],
+        ["STUWMEERREGELING", "AANTAL_BESLUIT_STUWMEER", "AANTAL_BESLUIT_STUWMEER",""],
+
+        ["aos_totaal","","",""],
+        ["OPNAMES","RAPPORTAGES","RAPPORTAGES",""],
+        ["OPNAMES","WACHT_OP_RAPPORT","IN_PROCEDURE","in-procedure"],
+        ["RAPPORTAGES","BESLUITEN","BESLUITEN",""],
+        ["RAPPORTAGES","WACHT_OP_BESLUIT","IN_PROCEDURE","in-procedure"],
+        ["BESLUITEN","AANTAL_AFGEWEZEN","AANTAL_AFGEWEZEN",""],
+        ["BESLUITEN","AANTAL_GEDEELTELIK_TOEGEK","AANTAL_GEDEELTELIK_TOEGEK",""],
+        ["BESLUITEN","AANTAL_GEENCONSISTENT_BES","AANTAL_GEENCONSISTENT_BES",""],
+        ["BESLUITEN","AANTAL_GEHEEL_TOEGEKEND","AANTAL_GEHEEL_TOEGEKEND",""],
+        ["AANTAL_AFGEWEZEN","","",""],
+        ["AANTAL_GEDEELTELIK_TOEGEK","","",""],
+        ["AANTAL_GEENCONSISTENT_BES","","",""],
+        ["AANTAL_GEHEEL_TOEGEKEND","","",""],
+        ["AANTAL_BESLUIT_STUWMEER","","",""],
+        ["IN_PROCEDURE","","",""]
+    ];
+
+    d3.json(url, {
+        method:"POST",
+        body: JSON.stringify({ 'connections' : connections }),
+        headers: { "Content-type": "application/json; charset=UTF-8"}
+    }).then( ({nodes,links}) => {
 
         function draw() {}
 
