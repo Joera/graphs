5f5282531553923710cca508c73a2909fbb6aacb
Joera <joeramulders@gmail.com>
2019-09-18 15:50:37 +0200

    breedtes balkjes

--- src/chart-y-scale.js
+++ src/chart-y-scale.js
@@ -16,30 +16,36 @@ let ChartYScale = function ChartYScale(config,dimensions,scale) {
         console.log(dimensions);
 
         scale.linear = d3.scaleLinear()
-            .range([dimensions.height, 0])
             .domain([
                 minValue,
                 config.maxValue || d3.max(data, d => d[property])
             ]).nice();
 
         scale.stacked = d3.scaleLinear()
-            .range([(config.fixedHeight || dimensions.height), 0])
             .domain([
                 minValue,
                 config.maxValue || d3.max(data, function (d) { if (d[d.length - 1] && d[d.length - 1][1]) { return d[d.length - 1][1]; }} )
             ]).nice();
 
-        scale.yBlocks = d3.scaleLinear()
-            .range([(config.maxValue / 100), 0]) // geen idee waarom 259 ipv 250
-            .domain([0,config.maxValue]);
+        // scale.yBlocks = d3.scaleLinear()
+        //     .range([(config.maxValue / 100), 0]) // geen idee waarom 259 ipv 250
+        //     .domain([0,config.maxValue]);
 
         return scale;
     }
 
 
-    let reset = function reset(dimensions,newScales) {
+    let reset = function reset(dimensions,newScale) {
 
-        return newScales;
+        newScale.linear
+            .range([dimensions.height, 0]);
+
+        newScale.stacked
+            .range([(config.fixedHeight || dimensions.height), 0]);
+
+
+
+        return newScale;
     }
 
 
