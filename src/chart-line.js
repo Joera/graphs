cba5c7f5889940061c1384208877a3e9233b8d13
Joera <joeramulders@gmail.com>
2019-09-19 19:14:52 +0200

    xx

--- src/chart-line.js
+++ src/chart-line.js
@@ -15,19 +15,19 @@ let ChartLine = function ChartLine(config,svg,dimensions) {
             .data([data])
             .attr("class", "line");
 
-        // svg.candlesUp = svg.layers.data.selectAll('.candle up')
-        //     .data(data)
-        //     .enter()
-        //     .append("rect")
-        //     .attr("class", "candle up")
-        //     .style("fill", "orange");
-        //
-        // svg.candlesDown = svg.layers.data.selectAll('.candle down')
-        //     .data(data)
-        //     .enter()
-        //     .append("rect")
-        //     .attr("class", "candle down")
-        //     .style("fill", "#777c00");
+        svg.candlesUp = svg.layers.data.selectAll('.candle up')
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "candle up")
+            .style("fill", "orange");
+
+        svg.candlesDown = svg.layers.data.selectAll('.candle down')
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "candle down")
+            .style("fill", "#777c00");
 
 
     }
@@ -45,27 +45,27 @@ let ChartLine = function ChartLine(config,svg,dimensions) {
         svg.line
             .attr("d", functions.line);
 
-        // svg.candlesUp
-        //     .attr('x',(d) => { return xScale.time(new Date(d[config.xParameter])); })
-        //     .attr('y',(d) => { return yScale.linear(d[config.yParameter]) - (yScale.linear(0) - yScale.linear(d.increase)) })
-        //     .attr('width',candleWidth)
-        //     .attr('height', (d) => { return yScale.linear(0) - yScale.linear(d.increase)  } )
-        //     .on("mouseover", function(d) {
-        //
-        //         svg.tooltip
-        //             .html(popup(d))
-        //             .style("left", (d3.event.pageX + 5) + "px")
-        //             .style("top", (d3.event.pageY - 5) + "px")
-        //             .transition()
-        //             .duration(250)
-        //             .style("opacity", 1);
-        //     })
-        //     .on("mouseout", function(d) {
-        //         svg.tooltip.transition()
-        //             .duration(250)
-        //             .style("opacity", 0);
-        //     })
-        // ;
+        svg.candlesUp
+            .attr('x',(d) => { return xScale.time(new Date(d[config.xParameter])); })
+            .attr('y',(d) => { return yScale.linear(d[config.yParameter]) - (yScale.linear(0) - yScale.linear(d['nieuwe_schademeldingen'])) })
+            .attr('width',candleWidth)
+            .attr('height', (d) => { return yScale.linear(0) - yScale.linear(d['nieuwe_schademeldingen'])  } )
+            .on("mouseover", function(d) {
+
+                svg.tooltip
+                    .html(popup(d))
+                    .style("left", (d3.event.pageX + 5) + "px")
+                    .style("top", (d3.event.pageY - 5) + "px")
+                    .transition()
+                    .duration(250)
+                    .style("opacity", 1);
+            })
+            .on("mouseout", function(d) {
+                svg.tooltip.transition()
+                    .duration(250)
+                    .style("opacity", 0);
+            })
+        ;
 
         // svg.candlesDown
         //     .attr('x',(d) => { return xScale.time(new Date(d[config.xParameter])); })
