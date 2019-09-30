3ea037f6975e8394c9ff56e6a089efe613a526f7
Joera <joeramulders@gmail.com>
2019-09-30 16:07:00 +0200

    yo

--- src/chart-line.js
+++ src/chart-line.js
@@ -76,9 +76,9 @@ let ChartLine = function ChartLine(config,svg,dimensions) {
 
                 svg.tooltip
                     .html(popup(d))
-                    .style("right", (d3.event.pageX + 5) + "px")
+                    .style("left", (d3.event.pageX + 5) + "px")
                     .style("top", (d3.event.pageY - 5) + "px")
-                    .attr('width', 400)
+                    // .style('width', '400px')
                     .transition()
                     .duration(250)
                     .style("opacity", 1);
