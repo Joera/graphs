4d6b81a8797f65cc352ab3813027f71ce5cfb98a
Joera <joeramulders@gmail.com>
2019-10-01 13:00:46 +0200

    inspiratiebord

--- src/default/meldingen.js
+++ src/default/meldingen.js
@@ -17,7 +17,7 @@ var meldingen = function(element,customConfig) {
     config.margin.right = 0;
     config.padding.top = 30;
     config.padding.bottom = 30;
-    config.padding.left = 30;
+    config.padding.left = 0;
     config.padding.right = 0;
     // name of first column with values of bands on x axis
 
