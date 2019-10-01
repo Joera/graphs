3148fcd9855c167a82e951dd76c7347eb261748d
Joera <joeramulders@gmail.com>
2019-10-01 13:26:38 +0200

    inspiratiebord

--- src/default/meldingen.js
+++ src/default/meldingen.js
@@ -32,7 +32,7 @@ var meldingen = function(element,customConfig) {
    // config.minWidth = 460;
 
     config.paddingInner = 0;
-    config.paddingOuter = .075;
+    config.paddingOuter = .2;
 
     config.dataArrayLength = 4;
 
