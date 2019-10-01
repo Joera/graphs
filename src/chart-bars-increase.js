f6e0ea8576febf4ff74b0a836a256ca9e5978398
Joera <joeramulders@gmail.com>
2019-10-01 12:46:21 +0200

    inspiratiebord

--- src/chart-bars-increase.js
+++ src/chart-bars-increase.js
@@ -189,7 +189,7 @@ let ChartBarsIncrease = function ChartBarsIncrease(config,svg,functions) {
             .merge(svg.differenceCircleEnter)
             .attr("cy", function(d) { return yScale.linear(d[property]) + (barWidth / 2); })
             .attr("cx", function(d) {
-                return xScale.band(d[config.xParameter]) + (barWidth / 2) + 3;
+                return xScale.band(d[config.xParameter]) + (barWidth / 2);
             })
             .attr("r", (barWidth / 2) - 10)
             .attr('fill-opacity', 0)
