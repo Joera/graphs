ac0e82deac41268b06d507d926a8287956711521
Joera <joeramulders@gmail.com>
2019-10-02 20:30:31 +0200

    inspiratiebord

--- src/default/pie-chart-sum.js
+++ src/default/pie-chart-sum.js
@@ -80,7 +80,7 @@ var pieChartSum = function(element,smallMultiple) {
             // if (window.innerWidth < 640) {
 
             svg.layers.legend
-                .attr('transform', 'translate(' + 100 + ',' + (dimensions.containerHeight / 2) + ')');
+                .attr('transform', 'translate(' + 100 + ',' + 30 + ')');
 
                 data.forEach( (d,i) => {
 
