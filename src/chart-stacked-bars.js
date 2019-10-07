07ba06984edc5d302aab7adf5cbbc42e70a391b8
Joera <joeramulders@gmail.com>
2019-10-07 14:39:52 +0200

    yo

--- src/chart-stacked-bars.js
+++ src/chart-stacked-bars.js
@@ -154,7 +154,7 @@ let ChartStackedBars = function ChartStackedBars(config,svg,functions) {
             })
             .attr('transform', function(d) {
 
-                let yOffset = smallMultiple ? dimensions.height : dimensions.height + 25;
+                let yOffset = smallMultiple ? dimensions.height : dimensions.height + 20;
 
                 return 'translate(' + (xScale.band(d[config.xParameter]) + (barWidth / 2))  + ','
                     + yOffset + ')';
