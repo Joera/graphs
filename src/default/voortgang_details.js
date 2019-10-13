17469a7e2cf338efc215eb15d4674b0d16fa94f0
Joera <joeramulders@gmail.com>
2019-10-13 12:06:59 +0200

    yovaol

--- src/default/voortgang_details.js
+++ src/default/voortgang_details.js
@@ -14,11 +14,11 @@ var voortgangDetails = function(element,smallMultiple) {
 
     config.margin.top = 0;
     config.margin.bottom = 0;
-    config.margin.left = 30;
+    config.margin.left = 60;
     config.margin.right = 0;
     config.padding.top = smallMultiple ? 15 : 30;
     config.padding.bottom = 30;
-    config.padding.left = 60;
+    config.padding.left = 30;
     config.padding.right = 0;
     // name of first column with values of bands on x axis
 
