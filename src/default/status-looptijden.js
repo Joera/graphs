883e0a667a260eece19fb763b62159d0662ffcb6
Joera <joeramulders@gmail.com>
2019-10-25 15:57:56 +0200

    zz

--- src/default/status-looptijden.js
+++ src/default/status-looptijden.js
@@ -33,8 +33,8 @@ var statusLooptijden  = function (element,smallMultiple) {
     // x-axis
     // config.minWidth = 460;
     config.xParameter = 'status';
-    config.paddingInner = [0.2];
-    config.paddingOuter = [0.2];
+    config.paddingInner = [0.1];
+    config.paddingOuter = [0.1];
 
     config.dateLabels = false;
 
