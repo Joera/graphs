e9b3bfa54f82287d52ef03b885be3204a8d5a7c0
Joera <joeramulders@gmail.com>
2019-10-28 11:08:48 +0100

    zz

--- src/default/looptijden-status.js
+++ src/default/looptijden-status.js
@@ -40,10 +40,11 @@ var looptijdenStatus  = function (element,smallMultiple) {
 
     let colours = {
 
-        'langer_dan_twee': 'orange',
-        'tussen_een_en_twee': 'green',
-        'tussen_half_en_een': 'blue',
-        'minder_dan_half': 'darkblue'
+        'ontvangst': 'orange',
+        'planning_opname': 'green',
+        'opleveren_schaderapport': 'blue',
+        'voorbereiden_commissie': 'darkblue',
+        'stuwmeer': 'grey'
     }
 
     // get dimensions from parent element
@@ -68,68 +69,51 @@ var looptijdenStatus  = function (element,smallMultiple) {
         let data = [];
 
         data.push({
-            status: "Ontvangst en analyse",
-            totaal: json['ONTVANGST'],
-            langer_dan_twee: json['LANGER_2_JAAR_ONTVANGST'],
-            tussen_een_en_twee: json['TUSSEN_1_2_JAAR_ONTVANGST'],
-            tussen_half_en_een: json['HALF_JAAR_1JAAR_ONTVANGST'],
-            minder_dan_half: json['MNDER_HALF_JAAR_ONTVANGST']
+            status: "Langer dan twee jaar",
+            ontvangst: json['LANGER_2_JAAR_ONTVANGST'],
+            planning_opname: json['LANGER_2_JAAR_PLANNING_OPNAME'],
+            opleveren_schaderapport: json['LANGER_2_JAAR_OPLEV_SCHRAP'],
+            voorbereiden_commissie: json['LANGER_2_JAAR_VOORBER_CIE'],
+            stuwmeer: json['LANGER_2_JAAR_STATUS_STUW']
         });
 
         data.push({
-            status: "Schade-opname wordt ingepland",
-            totaal: json['PLANNING_OPNAME'],
-            langer_dan_twee: json['LANGER_2_JAAR_PLANNING_OPNAME'],
-            tussen_een_en_twee: json['TUSSEN_1_2_JAAR_PLANNING_OPNAME'],
-            tussen_half_en_een: json['HALF_JAAR_1JAAR_PLANNING_OPNAME'],
-            minder_dan_half: json['MINDER_HALF_JAAR_PLANNING']
-        });
-
-
-        data.push({
-            status: "Schade-opname uitgevoerd, adviesrapport opleveren",
-            totaal: json['OPLEV_SCHADERAPPORT'],
-            langer_dan_twee: json['LANGER_2_JAAR_OPLEV_SCHRAP'],
-            tussen_een_en_twee: json['TUSSEN_1_2_JAAR_OPLEV_SCHRAP'],
-            tussen_half_en_een: json['HALF_JAAR_1JAAR_OPLEV_SCHRAP'],
-            minder_dan_half: json['MINDER_HALF_JAAR_OPLEV_SCHRAP']
+            status: "Tussen een en twee jaar",
+            ontvangst: json['TUSSEN_1_2_JAAR_ONTVANGST'],
+            planning_opname: json['TUSSEN_1_2_JAAR_PLANNING_OPNAME'],
+            opleveren_schaderapport: json['TUSSEN_1_2_JAAR_OPLEV_SCHRAP'],
+            voorbereiden_commissie: json['TUSSEN_1_2_JAAR_VOORBER_CIE'],
+            stuwmeer: json['TUSSEN_1_2_JAAR_STATUS_STUW']
         });
 
 
         data.push({
-            status: "Adviesrapport opgeleverd, besluit voorbereiden",
-            totaal: json['VOORBER_CIE'],
-            langer_dan_twee: json['LANGER_2_JAAR_VOORBER_CIE'],
-            tussen_een_en_twee: json['TUSSEN_1_2_JAAR_VOORBER_CIE'],
-            tussen_half_en_een: json['HALF_JAAR_1JAAR_VOORBER_CIE'],
-            minder_dan_half: json['MINDER_HALF_JAAR_VOORBER_']
+            status: "Tussen half jaar en een jaar",
+            ontvangst: json['HALF_JAAR_1JAAR_ONTVANGST'],
+            planning_opname: json['HALF_JAAR_1JAAR_PLANNING_OPNAME'],
+            opleveren_schaderapport: json['HALF_JAAR_1JAAR_OPLEV_SCHRAP'],
+            voorbereiden_commissie: json['HALF_JAAR_1JAAR_VOORBER_CIE'],
+            stuwmeer: json['HALF_JAAR_1JAAR_STATUS_STUW']
         });
+
 
         data.push({
-            status: "Stuwmeerregeling",
-            totaal: json['BESCHIKT'],
-            langer_dan_twee: json['LANGER_2_JAAR_STATUS_STUW'],
-            tussen_een_en_twee: json['TUSSEN_1_2_JAAR_STATUS_STUW'],
-            tussen_half_en_een: json['HALF_JAAR_1JAAR_STATUS_STUW'],
-            minder_dan_half: json['MINDER_HALF_JAAR_STATUS_STUW']
+            status: "Minder dan een half jaar",
+            ontvangst: json['MNDER_HALF_JAAR_ONTVANGST'],
+            planning_opname: json['MINDER_HALF_JAAR_PLANNING'],
+            opleveren_schaderapport: json['MINDER_HALF_JAAR_OPLEV_SCHRAP'],
+            voorbereiden_commissie: json['MINDER_HALF_JAAR_VOORBER_'],
+            stuwmeer: json['MINDER_HALF_JAAR_STATUS_STUW']
         });
 
-        data.push({
-            status: "Besluit genomen",
-            totaal: json['BESCHIKT'],
-            langer_dan_twee: 0,
-            tussen_een_en_twee: 0,
-            tussen_half_en_een: 0,
-            minder_dan_half: json['BESCHIKT']
 
-        });
 
         console.log(data);
 
 
         functions.stack = d3.stack()
             .keys(Object.keys(data[0]).filter(key => {
-                return ['status','totaal'].indexOf(key) < 0
+                return ['status'].indexOf(key) < 0
             } ));
 
         let stackedData = functions.stack(data);
