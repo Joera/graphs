3c6058868b11c70c5ffb751a8b94200f79359c4c
Joera <joeramulders@gmail.com>
2019-10-28 16:53:00 +0100

    zz

--- src/default/looptijden-status.js
+++ src/default/looptijden-status.js
@@ -38,7 +38,7 @@ var looptijdenStatus  = function (element,smallMultiple) {
 
     config.dateLabels = false;
 
-    config.fixedHeight = 240;
+    config.fixedHeight = 280;
 
     let colours = {
 
