4dfea2398d41387f3d658182cd2ce29f81dbc827
Joera <joeramulders@gmail.com>
2019-11-04 13:17:57 +0100

    specials

--- src/chart-bar-vertical.js
+++ src/chart-bar-vertical.js
@@ -54,7 +54,7 @@ let ChartBarVertical = function ChartBarVertical(config,svg) {
 
         svg.bar
             .merge(svg.barEnter)
-            .attr("x", 120)
+            .attr("x", config.padding.left)
             .attr("y", function(d,i) { return (i *  barHeight) + (i * barSpacing)})
             .attr("height", barHeight)
             .attr("width", 0)
