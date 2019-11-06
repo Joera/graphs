a24c988e4651e1d0a3393689b99eca5503a033fa
Joera <joeramulders@gmail.com>
2019-11-06 17:32:31 +0100

    jovel

--- src/chart-y-scale.js
+++ src/chart-y-scale.js
@@ -93,7 +93,7 @@ let ChartYScale = function ChartYScale(config,dimensions,scale) {
             .range([0,dimensions.height]);
 
         newScale.radius
-            .range([10, (dimensions.width / 10)]);
+            .range([6, (dimensions.width / 8)]);
 
         return newScale;
     }
