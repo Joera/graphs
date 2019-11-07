447f0de444cbcc0ff34b1364758afe110acf3ce6
Joera <joeramulders@gmail.com>
2019-11-07 16:44:43 +0100

    scales

--- src/default/force-looptijden-status.js
+++ src/default/force-looptijden-status.js
@@ -29,7 +29,7 @@ var forceLooptijdenStatus  = function (element,smallMultiple) {
 
     config.dateLabels = false;
 
-    config.radiusOffset = 2;
+    config.radiusOffset = 2.1;
     config.radiusFactor = 11;
 
     let colours = {
