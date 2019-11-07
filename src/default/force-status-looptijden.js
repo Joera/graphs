bf4b6cd7cdba84735c84d810aa11f1896a0ad8e3
Joera <joeramulders@gmail.com>
2019-11-07 12:32:18 +0100

    scales

--- src/default/force-status-looptijden.js
+++ src/default/force-status-looptijden.js
@@ -332,7 +332,7 @@ var forceStatusLooptijden  = function (element,smallMultiple) {
 
                 simulation[group[0].value]
                     .velocityDecay(0.2)
-                    .force('x', d3.forceX().strength(forceStrength).x(center.x))
+                 //   .force('x', d3.forceX().strength(forceStrength).x(center.x))
                     .force('y', d3.forceY().strength(forceStrength).y(center.y))
                     .force('charge', d3.forceManyBody().strength(cluster))
                     .on('tick', ticked);
