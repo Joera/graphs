4fc6d2e2f6ef8cad1720ceaead503e852aaf89c8
Joera <joeramulders@gmail.com>
2019-11-07 10:54:52 +0100

    scales

--- src/default/force-status-looptijden.js
+++ src/default/force-status-looptijden.js
@@ -214,7 +214,7 @@ var forceStatusLooptijden  = function (element,smallMultiple) {
         svg.headers_lines = svg.groupEnter.merge(svg.group)
             .append("rect")
             .attr('width',1)
-            .style('fill','#ddd');
+            .style('fill','#ccc');
 
         svg.circles.exit().remove();
 
@@ -290,7 +290,7 @@ var forceStatusLooptijden  = function (element,smallMultiple) {
 
 
         svg.headers_lines
-            .attr('height', (d,i) => (i % 2 == 0) ? 120 : 80)
+            .attr('height', (d,i) => (i % 2 == 0) ? 104 : 80)
             .attr('y', (d,i) => (i % 2 == 0) ? 6 : 30)
             .attr('x', groupWidth / 2)
 
