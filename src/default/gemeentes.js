14392f31ee9288c1eaff1b01b2386a08b0253dbe
Joera <joeramulders@gmail.com>
2019-11-13 14:32:39 +0100

    playin with datamapping

--- src/default/gemeentes.js
+++ src/default/gemeentes.js
@@ -130,8 +130,6 @@ var gemeentes = function(element,dataMapping,property) {
 
         window.addEventListener("resize", redraw(features, property), false);
 
-        console.log('ff');
-
         for (let radio of radios) {
             radio.addEventListener( 'change', () => {
                 console.log(radio.value);
