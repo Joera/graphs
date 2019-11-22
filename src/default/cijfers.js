8da001cf88683e802a0453500823e97bf938e4b0
Joera <joeramulders@gmail.com>
2019-11-22 12:52:16 +0100

    inspiratie

---
+++ src/default/cijfers.js
@@ -0,0 +1,9 @@
+var cijfers = function(element,dataMapping,property,smallMultiple) {
+
+    let div = document.createElement('div');
+
+    div.innerHTML('42');
+
+    element.appendChild(div);
+
+}
