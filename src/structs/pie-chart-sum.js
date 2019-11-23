56bd1c09418acdef659f84f595256662895446d5
Joera <joeramulders@gmail.com>
2019-11-23 21:07:04 +0100

    inspiratie

--- src/structs/pie-chart-sum.js
+++ src/structs/pie-chart-sum.js
@@ -194,7 +194,7 @@ class PieChartSum  {
         this.redraw();
         this.legend(data,segment);
 
-        this.element.addEventListener("resize", function() { self.redraw() }, false);
+        window.addEventListener("resize", function() { self.redraw() }, false);
 
         if(this.municipalitySelect != null) {
             this.municipalitySelect.addEventListener("change", function () {
