7c2cf2271a24c9cea295c47f7efad22ad98edfaf
Joera <joeramulders@gmail.com>
2019-11-23 12:05:22 +0100

    inspiratie

--- src/default/cijfers.js
+++ src/default/cijfers.js
@@ -3,7 +3,7 @@ class Cijfers {
 
     constructor(element,dataMapping,property,segment,smallMultiple) {
 
-        this.element = document.querySelector(element);
+        this.element = (typeof element === 'string') ? document.querySelector(element) : element;
         this.dataMapping = dataMapping;
         this.property = property;
         this.smallMultiple = smallMultiple;
