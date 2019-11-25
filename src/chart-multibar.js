3dce516e648ca2fd04f8529b6af764de0acc4d25
Joera <joeramulders@gmail.com>
2019-11-25 10:41:34 +0100

    inspiratie

--- src/chart-multibar.js
+++ src/chart-multibar.js
@@ -26,21 +26,21 @@ let ChartMultiBars = function ChartMultiBars(config,svg) {
                 return "bar  ";  // + colours[i]; // + sluggify(d.status) + "
             });
 
-        svg.barLabels = svg.layers.data.selectAll(".barLabel")
-            .data(data);
-
-        svg.barLabels.exit().remove();
-
-        svg.barLabelsEnter = svg.barLabels
-            .enter()
-            .append('text')
-            .attr('class','barLabel small-label')
-            .attr('x', 0)
-            .attr('dx', '0px')
-            .attr('dy', '-6px')
-            .style("text-anchor", "middle")
-
-            ;
+        // svg.barLabels = svg.layers.data.selectAll(".barLabel")
+        //     .data(data);
+        //
+        // svg.barLabels.exit().remove();
+        //
+        // svg.barLabelsEnter = svg.barLabels
+        //     .enter()
+        //     .append('text')
+        //     .attr('class','barLabel small-label')
+        //     .attr('x', 0)
+        //     .attr('dx', '0px')
+        //     .attr('dy', '-6px')
+        //     .style("text-anchor", "middle")
+        //
+        //     ;
     }
 
     let redraw = function redraw(dimensions,xScale,yScale) {
@@ -78,41 +78,41 @@ let ChartMultiBars = function ChartMultiBars(config,svg) {
 
         ;
 
-        svg.barLabels
-            .merge(svg.barLabelsEnter)
-            .text(function(d) {
-
-                if(config.currencyLabels) {
-
-                    return convertToCurrency(d[config.yParameter]);
-
-                } else {
-
-                    return d[config.yParameter] ? d[config.yParameter] : '< 25';
-                }
-
-
-            })
-            .attr('transform', function(d) {
-
-                if (config.xParameter === '_date') {
-
-                    return 'translate(' + (xScale.time(new Date(d[config.xParameter]))) + 60 + ',' +
-                        yScale.linear(d[config.yParameter])
-                        + ')';
-
-                } else {
-
-                    return 'translate(' + (xScale.band(d[config.xParameter]) + (xScale.band.bandwidth() / 2)) + ',' +
-                        yScale.linear(d[config.yParameter])
-                        + ')';
-                }
-            })
-            .attr('fill-opacity', 0)
-            .transition()
-            .delay(500)
-            .duration(500)
-            .attr('fill-opacity', 1)
+        // svg.barLabels
+        //     .merge(svg.barLabelsEnter)
+        //     .text(function(d) {
+        //
+        //         if(config.currencyLabels) {
+        //
+        //             return convertToCurrency(d[config.yParameter]);
+        //
+        //         } else {
+        //
+        //             return d[config.yParameter] ? d[config.yParameter] : '< 25';
+        //         }
+        //
+        //
+        //     })
+        //     .attr('transform', function(d) {
+        //
+        //         if (config.xParameter === '_date') {
+        //
+        //             return 'translate(' + (xScale.time(new Date(d[config.xParameter]))) + 60 + ',' +
+        //                 yScale.linear(d[config.yParameter])
+        //                 + ')';
+        //
+        //         } else {
+        //
+        //             return 'translate(' + (xScale.band(d[config.xParameter]) + (xScale.band.bandwidth() / 2)) + ',' +
+        //                 yScale.linear(d[config.yParameter])
+        //                 + ')';
+        //         }
+        //     })
+        //     .attr('fill-opacity', 0)
+        //     .transition()
+        //     .delay(500)
+        //     .duration(500)
+        //     .attr('fill-opacity', 1)
     }
 
 
