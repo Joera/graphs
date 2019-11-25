36b3932ef1211c30be24edc31b10eddeeb057816
Joera <joeramulders@gmail.com>
2019-11-25 22:59:37 +0100

    inspiratie

--- src/structs/bar-progression.js
+++ src/structs/bar-progression.js
@@ -30,7 +30,7 @@ class BarProgression  {
         this.config.margin.right = 0;
         this.config.padding.top = 10;
         this.config.padding.bottom = 15;
-        this.config.padding.left = 30;
+        this.config.padding.left = 0;
         this.config.padding.right = 0;
 
         this.config.minValue = 26000;
