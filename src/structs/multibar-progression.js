361d4279d1a1597cceb4dbb3b0fe4bb212ce19bb
Joera <joeramulders@gmail.com>
2019-11-25 15:30:13 +0100

    inspiratie

--- src/structs/multibar-progression.js
+++ src/structs/multibar-progression.js
@@ -45,7 +45,7 @@ class MultiBarProgression  {
         this.config.paddingInner = 0;
         this.config.paddingOuter = 0;
 
-        this.config.barWidth = 14;
+        this.config.barWidth = 12;
 
         if (this.smallMultiple) {
             this.config.dataArrayLength = 7;
