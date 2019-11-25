1044ded1bd9d69f4aae3c24d30c57451f234d93a
Joera <joeramulders@gmail.com>
2019-11-25 14:33:39 +0100

    inspiratie

--- src/structs/multibar-progression.js
+++ src/structs/multibar-progression.js
@@ -121,7 +121,7 @@ class MultiBarProgression  {
 
         let elWidth = d3.select(this.elementID).node().getBoundingClientRect().width;
 
-        let arrayLength = 2 * Math.floor(elWidth / 2 * minBarWidth);
+        let arrayLength = 2 * Math.floor(elWidth / (2 * minBarWidth));
 
         data = data.slice(data.length - arrayLength,data.length);
 
