6994806149dcf8599b81949958d948afa1b7e328
Joera <joeramulders@gmail.com>
2019-12-02 14:22:15 +0100

    trendLine

--- src/chart-y-scale.js
+++ src/chart-y-scale.js
@@ -27,6 +27,7 @@ let ChartYScale = function ChartYScale(config,dimensions,scale) {
             }
         }
 
+        console.log(arrayOfCumulatedValues);
 
         scale.stacked = d3.scaleLinear()
             .domain([
