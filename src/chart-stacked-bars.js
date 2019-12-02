32dc86e2d712b99cfcac5ad012ec1df69ccdcffc
Joera <joeramulders@gmail.com>
2019-12-02 14:41:11 +0100

    trendLine

--- src/chart-stacked-bars.js
+++ src/chart-stacked-bars.js
@@ -125,51 +125,51 @@ let ChartStackedBars = function ChartStackedBars(config,svg,functions) {
             })
            ;
 
-        // svg.barLabels
-        //     .merge(svg.barLabelsEnter)
-        //     .text(function(d) {
-        //         if(thousands(d[1] - d[0]) > 0) {
-        //             return thousands(d[1] - d[0]);
-        //         }
-        //     })
-        //     .attr('transform', function(d) {
-        //
-        //         xOffset = dimensions.width / (2 * dataArray.length);
-        //         let start = (d[0] < config.minValue) ? config.minValue : d[0];
-        //         yOffset = ((yScale.stacked(d[1]) - yScale.stacked(start)) / 2) - 11;
-        //
-        //         return 'translate(' + (xScale.band(d.data[config.xParameter]) + ( xScale.band.bandwidth() / 2)) + ',' +
-        //             (yScale.stacked(d[1]) - yOffset)
-        //             + ')';
-        //     })
-        //     .attr('fill-opacity', 0)
-        //     .transition()
-        //     .delay(500)
-        //     .duration(500)
-        //     .attr('fill-opacity', 1);
+        svg.barLabels
+            .merge(svg.barLabelsEnter)
+            .text(function(d) {
+                if(thousands(d[1] - d[0]) > 0) {
+                    return thousands(d[1] - d[0]);
+                }
+            })
+            .attr('transform', function(d) {
+
+                xOffset = dimensions.width / (2 * dataArray.length);
+                let start = (d[0] < config.minValue) ? config.minValue : d[0];
+                yOffset = ((yScale.stacked(d[1]) - yScale.stacked(start)) / 2) - 11;
+
+                return 'translate(' + (xScale.band(d.data[config.xParameter]) + ( xScale.band.bandwidth() / 2)) + ',' +
+                    (yScale.stacked(d[1]) - yOffset)
+                    + ')';
+            })
+            .attr('fill-opacity', 0)
+            .transition()
+            .delay(500)
+            .duration(500)
+            .attr('fill-opacity', 1);
 
 
-        // if(config.dateLabels) {
-        //
-        //     svg.dateLabels
-        //         .merge(svg.dateLabelsEnter)
-        //         .text(function (d) {
-        //
-        //             // if (window.innerWidth < 900) {
-        //             return new Date(d['_date']).toLocaleDateString('nl-NL', {month: 'numeric', day: 'numeric'});
-        //             // } else {
-        //             //     return new Date(d['_date']).toLocaleDateString('nl-NL', {month: 'long', day: 'numeric'});
-        //             // }
-        //         })
-        //         .attr('transform', function (d) {
-        //
-        //             let yOffset = smallMultiple ? dimensions.height : dimensions.height + 20;
-        //
-        //             return 'translate(' + (xScale.band(d[config.xParameter]) + (xScale.band.bandwidth() / 2)) + ','
-        //                 + yOffset + ')';
-        //         })
-        //     ;
-        // }
+        if(config.dateLabels) {
+
+            svg.dateLabels
+                .merge(svg.dateLabelsEnter)
+                .text(function (d) {
+
+                    // if (window.innerWidth < 900) {
+                    return new Date(d['_date']).toLocaleDateString('nl-NL', {month: 'numeric', day: 'numeric'});
+                    // } else {
+                    //     return new Date(d['_date']).toLocaleDateString('nl-NL', {month: 'long', day: 'numeric'});
+                    // }
+                })
+                .attr('transform', function (d) {
+
+                    let yOffset = smallMultiple ? dimensions.height : dimensions.height + 20;
+
+                    return 'translate(' + (xScale.band(d[config.xParameter]) + (xScale.band.bandwidth() / 2)) + ','
+                        + yOffset + ')';
+                })
+            ;
+        }
 
     }
 
