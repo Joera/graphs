03093944f22e396070df0c659bfdca10df1fdae2
Joera <joeramulders@gmail.com>
2019-12-05 14:39:44 +0100

    yi

--- src/chart-circles.js
+++ src/chart-circles.js
@@ -63,7 +63,7 @@ let ChartCircles = function ChartCircles(config,svg,colours) {
         for (let group of data) {
             simulation[group[0].column] = d3.forceSimulation()
                 .velocityDecay(0.25)
-                .nodes(group.filter( (prop) =>  { console.log(prop); return prop.column !== 'status' }));
+                .nodes(group.filter( (prop) =>  { console.log(prop); return prop.column }));
         }
 
         svg.headers = svg.headerGroupEnter.merge(svg.headerGroup)
