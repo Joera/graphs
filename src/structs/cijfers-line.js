f70c9645f9041fcf16acdeaadcfe99e04ef58dd5
Joera <joeramulders@gmail.com>
2019-12-18 12:44:05 +0100

    vvv

--- src/structs/cijfers-line.js
+++ src/structs/cijfers-line.js
@@ -116,7 +116,7 @@ class CijfersLine  {
         // this.chartAxis.redrawXTimeAxis(this.dimensions,this.xScale,this.axes,false);
         // this.chartAxis.redrawYAxis(this.yScale,this.axes);
         // // redraw data
-        this.chartLine.redraw(this.dimensions,this.xScale,this.yScale);
+        this.chartLine.redraw(this.xScale,this.yScale,this.dimensions);
     }
 
     draw(data) {
