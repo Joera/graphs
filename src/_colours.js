9d8eec0493e6c5905379d23ef655c7869e2e10d6
Joera <joeramulders@gmail.com>
2019-12-18 15:03:16 +0100

    vvv

--- src/_colours.js
+++ src/_colours.js
@@ -7,12 +7,8 @@ const yellow = '#F8CD1C';
 
 
 const svgUp = `
-    <svg width="36px" height="95px" viewBox="0 0 36 95" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <g stroke="none" stroke-width="1" fill-rule="nonzero">
-                <polygon points="0.45 17.66 4.02 21.23 15.48 9.77 15.48 94.32 20.52 94.32 20.52 9.77 31.98 21.23 35.55 17.66 18 0.12"></polygon>
-        </g>
+    <svg width="62px" height="96px" viewBox="0 0 62 96" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <path d="M27.4,2.5 L1.3,45 C-0.4,47.9 1.6,51.5 4.9,51.5 L19.2,51.5 L19.2,92.1 C19.2,94 20.7,95.5 22.6,95.5 L39.3,95.5 C41.2,95.5 42.7,94 42.7,92.1 L42.7,51.5 L57,51.5 C60.3,51.5 62.4,47.8 60.6,45 L34.6,2.5 C33,-0.2 29,-0.2 27.4,2.5 Z" id="Shape"></path>
     </svg>
-
-
 `;
 
