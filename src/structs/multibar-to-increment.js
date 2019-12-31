f722e4d7ffe8a607a588f4608e6b9ea1d2dbf88f
Joera <joeramulders@gmail.com>
2019-12-31 11:49:06 +0100

    check dminesions

--- src/structs/multibar-to-increment.js
+++ src/structs/multibar-to-increment.js
@@ -69,7 +69,7 @@ class MultiBarWithIncrement  {
             d3.json(url, function(error, json) {
                 if (error) throw error;
                 globalData.weeks = json;
-                self.run(json,this.config.yDimension);
+                self.run(json,self.config.yDimension);
             });
         }
 
