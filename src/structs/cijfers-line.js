38ad816ed2cb58cbbdd3ec4351634deabceee719
Joera <joeramulders@gmail.com>
2020-01-23 11:59:54 +0100

    doorlooptijden

--- src/structs/cijfers-line.js
+++ src/structs/cijfers-line.js
@@ -130,7 +130,7 @@ class CijfersLine  {
 
         let avg = (data.reduce((a,b) => { console.log(data.length - 1); console.log(a + parseInt(b[this.property])); return a + parseInt(b[this.property]); },0)) / data.length - 1;
 
-        // console.log(avg);
+        console.log(avg);
 
         return avg;
     }
